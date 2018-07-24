b77c0e58c456db477274bff5809c212f237cf863
Roman Kotov <waydeveloper@gmail.com>
2018-07-24 22:05:40 +0300

    test ruby xcodeproj

---
+++ ExampleXcodeproj/ExampleXcodeproj/Test/Test2/AppDelegate/AppDelegate2.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  ExampleXcodeproj
+//
+//  Created by Roman Kotov on 24/07/2018.
+//  Copyright © 2018 Roman Kotov. All rights reserved.
+//
+
+import UIKit
+
+class View: UIView {
+    
+}
